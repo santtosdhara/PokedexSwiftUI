ccc9f643375e9b989cfb6230918d7903c529a03f
Dhara Santos <santtosdhara@gmail.com>
2025-08-27 09:30:59 -0400

    Initial Commit

---
+++ PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Dhara Chavez on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
